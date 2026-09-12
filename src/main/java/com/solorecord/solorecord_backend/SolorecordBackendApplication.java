3643c50cc3b68ace3900341e66e30198dd43bf5d
David-282 <afolabidavid94@gmail.com>
2026-09-12 12:25:09 +0100

    chore: scaffold spring boot project

---
+++ src/main/java/com/solorecord/solorecord_backend/SolorecordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.solorecord.solorecord_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolorecordBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolorecordBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solorecord/solorecord_backend/SolorecordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solorecord.solorecord_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolorecordBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
